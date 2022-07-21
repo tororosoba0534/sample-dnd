eadfd521e904d28620637cd475d42b60981aab0f
tekuteku11udana <tekuteku11udana888@gmail.com>
2022-07-21 19:35:55 +0900

    Add link to github repository.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,15 @@ import { DndList } from "./dnd/DndList";
 function App() {
   return (
     <div className="mx-20 my-10">
+      <div className="w-full h-20 lg:h-10 relative">
+        <a
+          href="https://github.com/tororosoba0534/sample-dnd"
+          target="_blank"
+          className="absolute right-5 top-5  rounded bg-gray-500 hover:bg-gray-400 text-white font-semibold flex items-center justify-center p-2  cursor-pointer"
+        >
+          View source code
+        </a>
+      </div>
       <DndList />
     </div>
   );
